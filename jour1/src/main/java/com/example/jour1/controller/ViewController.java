112d8e0d8b397252170233844d566e11d131ce49
mathieu-pinaud <mathieu.pinaud@laplateforme.io>
2023-09-20 10:32:48 +0200

    jour 2 fini

---
+++ jour1/src/main/java/com/example/jour1/controller/ViewController.java
@@ -0,0 +1,32 @@
+package com.example.jour1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.jour1.User;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class ViewController {
+    
+    @GetMapping("view")
+    public String view(Model model) {
+        List<String> items = List.of("Hello", "Spring", "Boot");
+        model.addAttribute("message", "Hello, Spring Boot!");
+        model.addAttribute("items", items);
+        return "view";
+    }
+
+     @PostMapping("view")
+    public String welcome(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("message", "Hello, Spring Boot!");
+        model.addAttribute("items", List.of("Hello", "Spring", "Boot"));
+        model.addAttribute("welcomeMessage", "Alors la zone ? " + user.getName() + " " + user.getAge());
+        return "view"; // Redirige vers la vue avec le message de bienvenue
+    }
+}
---
+++ jour2/src/main/java/com/example/jour2/User.java
@@ -0,0 +1,20 @@
+package com.example.jour2;
+
+public class User {
+    private String name;
+    private int age;
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+}
